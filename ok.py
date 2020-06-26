5c3cd90e16d5037f445d2c9f43d805e6da74af33
ThomDuff <thomaskilduff02@gmail.com>
2020-06-26 20:52:06 +0100

    second commit, iterate through pages

--- ok.py
+++ ok.py
@@ -1,8 +1,21 @@
 import bs4 as bs
 import urllib.request
 
-sauce = urllib.request.urlopen('https://www.daft.ie/kildare/property-for-sale/maynooth/?s%5Bmxp%5D=275000').read()
-soup = bs.BeautifulSoup(sauce,'lxml')
+county = input("Enter a county: ")
+city = input("Enter a city: ")
 
-for strongs in soup.find_all('strong', class_='PropertyInformationCommonStyles__costAmountCopy'):
-    print(strongs.text)
+pageNumber = 0
+for page in range(3):
+    print("\nPage number " + str(page+1))
+    
+    url = 'https://www.daft.ie/' + county + '/property-for-sale/' + city + '/?offset=' + str(pageNumber)
+
+    source = urllib.request.urlopen(url).read()
+
+    soup = bs.BeautifulSoup(source,'lxml')
+
+    for strongs in soup.find_all('strong', class_='PropertyInformationCommonStyles__costAmountCopy'):
+        print(strongs.text)
+    
+    pageNumber += 20
+    
