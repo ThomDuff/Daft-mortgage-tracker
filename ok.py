585cbbfe5a39b11b38ed6df5ae0902bf79195025
ThomDuff <thomaskilduff02@gmail.com>
2020-06-27 20:17:30 +0100

    third commit, rent option

--- ok.py
+++ ok.py
@@ -3,19 +3,40 @@ import urllib.request
 
 county = input("Enter a county: ")
 city = input("Enter a city: ")
+option = input("Do you want a house for sale, rent or to share? ")
 
-pageNumber = 0
-for page in range(3):
-    print("\nPage number " + str(page+1))
+if(option == "sale"):
+    pageNumber = 0
+    for page in range(3):
+        print("\nPage number " + str(page+1))
     
-    url = 'https://www.daft.ie/' + county + '/property-for-sale/' + city + '/?offset=' + str(pageNumber)
+        url = 'https://www.daft.ie/' + county + '/property-for-sale/' + city + '/?offset=' + str(pageNumber)
+        source = urllib.request.urlopen(url).read()
+        soup = bs.BeautifulSoup(source,'lxml')
 
+        for strongs in soup.find_all('strong', class_='PropertyInformationCommonStyles__costAmountCopy'):
+            print(strongs.text)
+        
+        pageNumber += 20
+
+if(option == "rent"):
+#    print("\nPage number " + str(page+1))
+    
+    url = 'https://www.daft.ie/' + county + '/rooms-to-share/' + city
     source = urllib.request.urlopen(url).read()
-
     soup = bs.BeautifulSoup(source,'lxml')
 
-    for strongs in soup.find_all('strong', class_='PropertyInformationCommonStyles__costAmountCopy'):
+    for strongs in soup.find_all('strong', class_='price'):
         print(strongs.text)
+                        
+#    pageNumber += 20
+
     
-    pageNumber += 20
-    
+
+
+#https://www.daft.ie/' + county + '/rooms-to-share/' + city + '/?offset=' + str(pageNumber)
+#    pageNumber = 0
+#    for page in range(3):
+
+
+#MUST ADD DYNAMIC PAGE NUMBER
